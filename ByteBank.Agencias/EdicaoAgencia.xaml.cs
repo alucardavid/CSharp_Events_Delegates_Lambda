d1d384c8d28e9f8612bce40205559598aacd2529
David Pereira <d_pereira@outlook.com.br>
2018-07-18 14:19:49 -0300

    Expressão lambda para atribuir metodo anonimo

--- ByteBank.Agencias/EdicaoAgencia.xaml.cs
+++ ByteBank.Agencias/EdicaoAgencia.xaml.cs
@@ -34,23 +34,14 @@ namespace ByteBank.Agencias
 
         private void AtualizarControles()
         {
-            RoutedEventHandler dialogResultTrue = delegate (object o, RoutedEventArgs e)
-            {
-                DialogResult = true;
-            };
-            RoutedEventHandler dialogResultFalse = delegate (object o, RoutedEventArgs e)
-            {
-                DialogResult = false;
-            };
+            RoutedEventHandler dialogResultTrue = (o, e) => DialogResult = true;
+            RoutedEventHandler dialogResultFalse = (o, e) => DialogResult = false;
 
             var okEventHandler = dialogResultTrue + Fechar;
             var cancelarEventHandler = dialogResultFalse + Fechar;
 
             btnOk.Click += okEventHandler;
             btnCancelar.Click += cancelarEventHandler;
-
-            btnOk.Click += new RoutedEventHandler(Fechar);
-            btnCancelar.Click += new RoutedEventHandler(Fechar);
 
         }
 
